8a4dca64c9cffd4426596f6635b3469ae5fdd239
Narender99 <81280034+Narender99@users.noreply.github.com>
2025-05-23 12:12:34 +0530

    project setup using parcel bundler

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const createRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+createRoot.render(<App />);
